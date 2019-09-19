91c3ba9d585dada5fd190081a7fe8c44880c6e93
JSkye3293 <35378464+JSkye3293@users.noreply.github.com>
2019-09-19 12:20:18 -0700

    Add files via upload

---
+++ About_me.py
@@ -0,0 +1,45 @@
+key = "Skye"
+e = input("Please state your name:")
+c = 0
+d = 0
+if e == key:
+    print("Welcome back, creator...")
+else:
+    if e == "Darth Vader":
+        while d == 0:
+            print("D E A R  G O D  R U N")
+            print("pleasehavemercy")
+            c = c + 1
+            if c == 15:
+                d = 1
+            
+    else:
+        e = e + "!"
+        print("Welcome "+ e + "!")
+        print(" ")
+        print("\n Today this program will introduce you to the crazy world of JSkye. \nA little about me: I am a programmer on my own time and love to work \nwith electronics, and have been doing so since I was little. \nSomething you didnt know about me is that I actually make music \nas well using computers. I also like to program NeoPixels and \nmaking them respond to music \nfor amazing and epic partys. \nAnd of course, sometimes just to say I can. : ) \nSomething I made, its my logo:")
+
+print("                ___")
+print("            ___/ //")
+print("         __/ ///")
+print("       _/ // __         __")
+print("      // /___||_________||________")
+print("        \ \ \ \ \ \ \ \ \ \ \ \ \ \ ")
+print("             ||         ||")
+print("       ______||_________/|_____ ")
+print("      / _____||_________||___  \ ")
+print("     / /     ||         ||   | |")
+print("     | |     ||         ||___| |")
+print("   __| |_____||_________| _____/___")
+print("  |\-| \_____|| -  -  - || -  -  - \  ")
+print("  | \_______//  -  -  - ||  -  -  - \  ")
+print("  |` \  -  -  - \T__E__C__H\ -  -  - \  ")
+print("  |`` \  -  -  -  -  -  -  -   2018   \ ")
+print("  |``` \_______________________________\  ")
+print("  |````|  ~   ~   ~   ~   ~   ~   ~   ~| ")
+print("  |````|~   ~   ~   ~   ~   ~   ~   ~  | ")
+print("  |````|  ~   ~   ~   ~   ~   ~   ~   ~| ")
+print("   ````|~   ~   ~   ~   ~   ~   ~   ~  | ")
+print("    ```|  ~   ~   ~   ~   ~   ~   ~   ~| ")
+print("     ``|~   ~   ~   ~   ~   ~   ~   ~  | ")
+print("      `|  ~   ~   ~   ~   ~   ~   ~   ~| ")
