122a3ec7690356282acf5cbf48538cbf592976eb
JSkye3293 <35378464+JSkye3293@users.noreply.github.com>
2019-09-19 14:00:18 -0700

    Add files via upload

---
+++ About_me.py
@@ -0,0 +1,86 @@
+key = "Skye"
+q = 0
+while q == 0:
+	e = input("Please state your name:")
+	c = 0
+	d = 0
+	a = 0
+	b = 0
+	if e == "exit":
+		print("Thank you!")
+		print("JTP_TECH")
+		q = 1
+		b = 1
+		a = 1
+		b = 1
+	if e == "EasterEggs":
+		print("Darth Vader \nSkye \nPeter \nexit")
+		b = 1
+		a = 1
+	if e == "help":
+		print("State your name to output a result. \nTo exit the program, type exit")
+		b = 1
+		a = 1
+	
+	if e == "": #blank input
+		e = "!"
+
+	if e == key:
+		print("Welcome back, creator...")
+		b = 1
+    
+
+	if e == "Darth Vader":
+		a = 1
+		while d == 0:
+			print("D E A R  G O D  R U N")
+			print("pleasehavemercy")
+			c = c + 1
+			if c == 15:
+				d = 1
+
+	if e == "Peter":
+		print(".....We've been expecting you, " + e + "...")
+		#print("\n Today this program will introduce you to the crazy world of JSkye. \nA little about me: I am a programmer on my own time and love to work \nwith electronics, and have been doing so since I was little. \nSomething you didnt know about me is that I actually make music \nas well using computers. I also like to program NeoPixels and \nmaking them respond to music \nfor amazing and epic partys. \nAnd of course, sometimes just to say I can. : ) \nSomething I made, its my logo:")
+		a = 1
+
+
+            
+	if a == 0:
+		if b == 0:
+			e = e + "!"
+			print("Welcome "+ e + "!")
+		print(" ")
+		print("\n Today this program will introduce you to the crazy world of JSkye. \nA little about me: I am a programmer on my own time and love to work \nwith electronics, and have been doing so since I was little. \nSomething you didnt know about me is that I actually make music \nas well using computers. I also like to program NeoPixels and \nmaking them respond to music \nfor amazing and epic partys. \nAnd of course, sometimes just to say I can. : ) \nSomething I made, its my logo:")
+		print("                ___ \n            ___/ // \n         __/ /// \n       _/ // __         __ \n      // /___||_________||________ \n        \ \ \ \ \ \ \ \ \ \ \ \ \ \ \n             ||         || \n       ______||_________/|_____ \n      / _____||_________||___  \ \n     / /     ||         ||   | | \n     | |     ||         ||___| | \n   __| |_____||_________| _____/___ \n  |\-| \_____|| -  -  - || -  -  - \  \n   | \_______//  -  -  - ||  -  -  - \  \n   | \_______//  -  -  - ||  -  -  - \  \n  |` \  -  -  - \T__E__C__H\ -  -  - \  \n  |`` \  -  -  -  -  -  -  -   2018   \ \n  |``` \_______________________________\  \n  |````|  ~   ~   ~   ~   ~   ~   ~   ~| \n  |````|~   ~   ~   ~   ~   ~   ~   ~  | \n  |````|  ~   ~   ~   ~   ~   ~   ~   ~| \n   ````|~   ~   ~   ~   ~   ~   ~   ~  | \n    ```|  ~   ~   ~   ~   ~   ~   ~   ~| \n     ``|~   ~   ~   ~   ~   ~   ~   ~  | \n      `|  ~   ~   ~   ~   ~   ~   ~   ~| ")
+
+if b == 0:
+	print("                ___ \n            ___/ // \n         __/ /// \n       _/ // __         __ \n      // /___||_________||________ \n        \ \ \ \ \ \ \ \ \ \ \ \ \ \ \n             ||         || \n       ______||_________/|_____ \n      / _____||_________||___  \ \n     / /     ||         ||   | | \n     | |     ||         ||___| | \n   __| |_____||_________| _____/___ \n  |\-| \_____|| -  -  - || -  -  - \  \n   | \_______//  -  -  - ||  -  -  - \  \n   | \_______//  -  -  - ||  -  -  - \  \n  |` \  -  -  - \T__E__C__H\ -  -  - \  \n  |`` \  -  -  -  -  -  -  -   2018   \ \n  |``` \_______________________________\  \n  |````|  ~   ~   ~   ~   ~   ~   ~   ~| \n  |````|~   ~   ~   ~   ~   ~   ~   ~  | \n  |````|  ~   ~   ~   ~   ~   ~   ~   ~| \n   ````|~   ~   ~   ~   ~   ~   ~   ~  | \n    ```|  ~   ~   ~   ~   ~   ~   ~   ~| \n     ``|~   ~   ~   ~   ~   ~   ~   ~  | \n      `|  ~   ~   ~   ~   ~   ~   ~   ~| ")
+
+
+
+
+#print("                ___")
+#print("            ___/ //")
+#print("         __/ ///")
+#print("       _/ // __         __")
+#print("      // /___||_________||________")
+#print("        \ \ \ \ \ \ \ \ \ \ \ \ \ \ ")
+#print("             ||         ||")
+#print("       ______||_________/|_____ ")
+#print("      / _____||_________||___  \ ")
+#print("     / /     ||         ||   | |")
+#print("     | |     ||         ||___| |")
+#print("   __| |_____||_________| _____/___")
+#print("  |\-| \_____|| -  -  - || -  -  - \  ")
+#print("  | \_______//  -  -  - ||  -  -  - \  ")
+#print("  |` \  -  -  - \T__E__C__H\ -  -  - \  ")
+#print("  |`` \  -  -  -  -  -  -  -   2018   \ ")
+#print("  |``` \_______________________________\  ")
+#print("  |````|  ~   ~   ~   ~   ~   ~   ~   ~| ")
+#print("  |````|~   ~   ~   ~   ~   ~   ~   ~  | ")
+#print("  |````|  ~   ~   ~   ~   ~   ~   ~   ~| ")
+#print("   ````|~   ~   ~   ~   ~   ~   ~   ~  | ")
+#print("    ```|  ~   ~   ~   ~   ~   ~   ~   ~| ")
+#print("     ``|~   ~   ~   ~   ~   ~   ~   ~  | ")
+#print("      `|  ~   ~   ~   ~   ~   ~   ~   ~| ")
